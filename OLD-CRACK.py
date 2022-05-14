e635aa34e8b2c5a90591ee2ddbdedfaa1e92beae
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2022-05-14 18:09:21 +0600

    Update OLD-CRACK.py

--- OLD-CRACK.py
+++ OLD-CRACK.py
@@ -1,7 +1,7 @@
 import os, sys, time
 from time import sleep
 try:
-    __import__('uid64.cpython-310.so').__main_apv__()
+    __import__('uid64').__main_apv__()
 except Exception as e:
     exit(str(e))
 
