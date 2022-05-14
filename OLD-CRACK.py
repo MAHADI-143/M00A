d4577e7d2ddd3a982bdcbb1f67f64601356eec8c
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2022-05-14 16:48:48 +0600

    Update OLD-CRACK.py

--- OLD-CRACK.py
+++ OLD-CRACK.py
@@ -1,6 +1,4 @@
-import os, sys, time
-from time import sleep
-try:
-    __import__('uid64.cpython-310.so').__niki_()
-except Exception as e:
-    exit(str(e))
+from uid64 import main
+
+main_apv()
+
