93c0fb2f84c532bb8af8e5429ceb869a708b1312
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2022-05-15 22:57:11 +0600

    Update OLD-CRACK.py

--- OLD-CRACK.py
+++ OLD-CRACK.py
@@ -1,7 +1,33 @@
-import os, sys, time
-from time import sleep
+import os, platform
+
 try:
-    __import__('uidd64').main_apv()
-except Exception as e:
-    exit(str(e))
+
+        import requests
+
+except:
+
+        os.system('pip2 install requests')
+
+ 
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == "64bit":
+
+        from uidd64 import xmmx
+
+        xmmx()
+
+elif bit == "32bit":
+
+        from uidd32 import maiin
+
+        maiin()
+
+
+
+
+
  
